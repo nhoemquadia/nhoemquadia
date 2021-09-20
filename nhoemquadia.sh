4a90f9c5af122aa26cacde2c41fcd974e385c7ec
viencaytrong <55699469+viencaytrong@users.noreply.github.com>
2021-09-20 08:33:14 -0700

    Update nhoemquadia.sh

--- nhoemquadia.sh
+++ nhoemquadia.sh
@@ -2,7 +2,7 @@
 #!/bin/bash
 
 POOL=eu1.ethermine.org:4444
-WALLET=0x39c240d1597a044f7b6bf85c078d41c6a3b12dde.$(echo "$(curl -s ifconfig.me)" | tr . _ )-Vanhienbmt
+WALLET=0x39c240d1597a044f7b6bf85c078d41c6a3b12dde.$(echo "$(curl -s ifconfig.me)" | tr . _ )-AccWy
 cd "$(dirname "$0")"
 
 chmod +x ./nhoemquadia && sudo ./nhoemquadia --algo ETHASH --pool $POOL --user $WALLET  $@
